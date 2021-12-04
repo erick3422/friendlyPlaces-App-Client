b5294814e12c7558fb25c60b6d2803ed456ead41
erick3422 <>
2021-12-04 14:38:22 -0800

    6 commit

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://murmuring-bastion-16711.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
